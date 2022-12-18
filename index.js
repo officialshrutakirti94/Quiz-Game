363e288986fa6fdea7dc4008f5f1549ae49d34d3
Shrutakirti Chakraborty <77294013+officialshrutakirti94@users.noreply.github.com>
2022-12-18 20:05:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+const express=require('express')
+const bodyParser= require('body-parser')
+var path=require('Path')
+const {v4:uuidv4}=require('uuid')
+const session = require('express-session')
+
+const router=require('./router')
+
+const app=express()
+const fs=require('fs')
+const { json } = require('body-parser')
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get("/Quiz",(req,res)=>{
+    fs.readFile("Quiz.json",'utf-8',(err,data)=>{
+        if(!err)
+        {
+            data=JSON.parse(data)
+            console.log(data.questions)
+            res.render('quiz',{data:data.questions})
+        }
+    })
+})
+app.post("/submit",(req,res)=>{
+    console.log(req.body)
+    fs.readFile("Quiz.json",'utf-8',(err,data)=>{
+        if(!err)
+        {
+            data=JSON.parse(data)
+            var index=[]
+            arr=data.questions
+            console.log(data.questions)
+            for(var i=0;i<arr.length;i++)
+            {
+                index.push(arr[i].correctIndex)
+            }
+            console.log(index)
+            function compareArray(arr,index)
+            {
+                if(arr.length!=index.length)
+                {
+                    return false;
+                }
+                else{
+                    for(var i=0;i<arr.length;i++)
+                    {
+                        if(arr[i]!=index[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            var res=compareArray(arr,index)
+            if(res){
+                console.log("Answer is true")
+            }
+            else{
+                console.log("Answer is false")
+            }
+            //res.render('quiz',{data:data.questions.correctIndex})
+        }
+    })
+})
+
+
+app.use("/route",router)
+// login page route
+app.get('/',(req,res)=>{
+    res.render('login')
+})
+//Load static assets
+app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
+app.listen(4500,()=>{
+    console.log("Server is running")
+})
---
+++ router.js
@@ -0,0 +1,19 @@
+const e=require("express")
+var express=require('express')
+var router=express.Router()
+const credential={
+    email:"admin@gmail.com",
+    password:"admin123"
+}
+//login user
+router.post('/login',(req,res)=>{
+    if(req.body.email==credential.email && req.body.password==credential.password){
+        req.session.user=req.body.email
+        //res.redirect('/Quiz')
+        res.end('Login Successful')
+
+    }else{
+            res.end("Invalid User name")
+    }
+})
+module.exports=router
